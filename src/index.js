f50f50c512ac0780134988c869ab4bf1a8cf73ab
Morris Witthein <morris@DESKTOP-B8KH39D.localdomain>
2022-05-11 13:32:41 +0200

    refactor: outsourced utility/logging functions in util.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ import { join } from 'path';
 import { Octokit } from 'octokit';
 import cliProgress from 'cli-progress';
 import Axios from 'axios';
-import * as winston from 'winston';
+import * as util from './util.js';
 
 var problems = '';
 // var copyrights = '';
 var githubClient;
-var logger;
 
 main();
 
 function main() {
     githubClient = createGithubClient();
-    logger = initializeLogger();
+    util.initializeLogger();
     if (githubClient == null) {
         return;
     }
@@ -25,41 +24,6 @@ function main() {
         insertCopyrightInformation(jsonData);
     } catch (err) {
         console.error(`Couldn't load bom.json from ${bomPath}.`);
-    }
-}
-
-function initializeLogger() {
-    return winston.createLogger({
-        levels: { license: 0, extRefs: 1 , copyright: 2},
-        format: winston.format.simple(),
-        transports: [
-            new winston.transports.File({ filename: 'combined.log', level: 'copyright' })
-        ],
-    });
-}
-
-function addToLog(packageInfo, level) {
-    let message = generateLogMessage(packageInfo, level);
-    if (message == '') {
-        return;
-    }
-    logger.log({
-        level: level,
-        message: message
-    });
-}
-
-function generateLogMessage(packageInfo, level) {
-    switch(level) {
-        case 'license':
-            return `No License found for: ${generatePackageName(packageInfo)}`;
-        case 'extRefs':
-            return `No external references found for: ${generatePackageName(packageInfo)}`
-        case 'copyright':
-            return `Unable to extract copyright notice for: ${generatePackageName(packageInfo)}`
-        default:
-            console.error(`Error: Unknown log level: ${level}. No log entry created for package: ${generatePackageName(packageInfo)}`);
-            return '';
     }
 }
 
@@ -82,18 +46,16 @@ async function insertCopyrightInformation(jsonData) {
         progBar.increment();
         let packageInfo = jsonData['components'][i];
         if (!hasLicense(packageInfo)) {
-            //TODO log packages without license
-            addToLog(packageInfo, 'license');
+            util.addToLog(packageInfo, 'license');
             continue;
         }
         if (!hasExternalRefs(packageInfo)) {
-            //TODO log packages without external refs
-            addToLog(packageInfo, 'extRefs');
+            util.addToLog(packageInfo, 'extRefs');
             continue;
         }
         let copyright = await retrieveCopyrightInformation(packageInfo);
         if (copyright == '') {
-            addToLog(packageInfo, 'copyright');
+            util.addToLog(packageInfo, 'copyright');
             continue;
         }
         insertCopyrightIntoBom(packageInfo, copyright);
@@ -132,7 +94,7 @@ function extractCopyright(license, packageInfo) {
         return re3.exec(license)[0];
         // copyrights += re3.exec(license)[0] + '\n';
     } else if (license.toLowerCase().includes('copyright')) {
-        problems += generatePackageName(packageInfo) + '\n';
+        problems += util.generatePackageName(packageInfo) + '\n';
         return '';
     }
     return '';
@@ -148,7 +110,6 @@ function hasExternalRefs(packageInfo) {
     return typeof packageInfo['externalReferences'] !== undefined;
 }
 
-
 async function retrieveCopyrightInformation(packageInfo) {
     const extRefs = packageInfo['externalReferences'];
     let license = null;
@@ -163,7 +124,7 @@ async function retrieveCopyrightInformation(packageInfo) {
             continue;
         }
         try {
-            let fileName = generatePackageName(packageInfo);
+            let fileName = util.generatePackageName(packageInfo);
             writeFileSync(join('out', 'licenses', `${fileName}.txt`), license);
         } catch (err) {
             console.error(err);
@@ -173,29 +134,7 @@ async function retrieveCopyrightInformation(packageInfo) {
             return copyright;
         }
     }
-    handleNoCopyrightFound(packageInfo);
     return '';
-}
-
-function generatePackageName(packageInfo) {
-    let fileName = '';
-    if (packageInfo['group'].trim() != '') {
-        fileName += packageInfo['group'] + '-';
-    }
-    if (packageInfo['name'].trim() != '') {
-        fileName += packageInfo['name'] + '-';
-    }
-    if (fileName === '') {
-        fileName = 'unnamed-';
-    }
-    if (packageInfo['version'].trim() != '') {
-        fileName += packageInfo['version'];
-    }
-    return fileName.charAt(fileName.length - 1) == '-' ? fileName.substring(0, fileName.length - 1) : fileName;;
-}
-
-function handleNoCopyrightFound(packageInfo) {
-    //TODO handle no copyright found
 }
 
 async function downloadLicenseFromGithub(url) {
---
+++ src/util.js
@@ -0,0 +1,55 @@
+import { createLogger, transports, format } from 'winston';
+
+let logger;
+
+export function initializeLogger() {
+    logger = createLogger({
+        levels: { license: 0, extRefs: 1, copyright: 2 },
+        format: format.simple(),
+        transports: [
+            new transports.File({ filename: 'combined.log', level: 'copyright' })
+        ],
+    });
+}
+
+export function addToLog(packageInfo, level) {
+    let message = generateLogMessage(packageInfo, level);
+    if (message == '') {
+        return;
+    }
+    logger.log({
+        level: level,
+        message: message
+    });
+}
+
+export function generatePackageName(packageInfo) {
+    let fileName = '';
+    if (packageInfo['group'].trim() != '') {
+        fileName += packageInfo['group'] + '-';
+    }
+    if (packageInfo['name'].trim() != '') {
+        fileName += packageInfo['name'] + '-';
+    }
+    if (fileName === '') {
+        fileName = 'unnamed-';
+    }
+    if (packageInfo['version'].trim() != '') {
+        fileName += packageInfo['version'];
+    }
+    return fileName.charAt(fileName.length - 1) == '-' ? fileName.substring(0, fileName.length - 1) : fileName;;
+}
+
+function generateLogMessage(packageInfo, level) {
+    switch (level) {
+        case 'license':
+            return `No License found for: ${generatePackageName(packageInfo)}`;
+        case 'extRefs':
+            return `No external references found for: ${generatePackageName(packageInfo)}`
+        case 'copyright':
+            return `Unable to extract copyright notice for: ${generatePackageName(packageInfo)}`
+        default:
+            console.error(`Error: Unknown log level: ${level}. No log entry created for package: ${generatePackageName(packageInfo)}`);
+            return '';
+    }
+}
