7c1d3ec9681a954d17aa554b3f204188113ce851
MorrisWitthein <morris.witthein@yahoo.de>
2022-05-13 12:57:53 +0200

    docs: added function comments

--- src/util.js
+++ src/util.js
@@ -1,8 +1,16 @@
 /* eslint-disable indent */
 import { createLogger, transports, format } from 'winston';
 
-let logger = null;
 
+let logger = null;
+/**
+ * Initializes the logger instance with used for documenting
+ * packages where no copyright notice could be found.
+ * Uses custom levels:
+ * license: no license information found in bom file
+ * extRefs: not external references found in bom file
+ * copyright: unable to extract copyright notice from external refs.
+ */
 export function initializeLogger() {
     logger = createLogger({
         levels: { license: 0, extRefs: 1, copyright: 2 },
@@ -12,7 +20,11 @@ export function initializeLogger() {
         ],
     });
 }
-
+/**
+ * Adds an entry to the log file.
+ * @param {object} packageInfo Entry from bom.json containing information for one package.
+ * @param {string} level The level of the log message.
+ */
 export function addToLog(packageInfo, level) {
     let message = generateLogMessage(packageInfo, level);
     if (message == '') {
@@ -23,7 +35,11 @@ export function addToLog(packageInfo, level) {
         message: message
     });
 }
-
+/**
+ * Generates a name for the given package in the form: "group-name-version".
+ * @param {object} packageInfo Entry from bom.json containing information for one package.
+ * @return {string} The generated name for the package.
+ */
 export function generatePackageName(packageInfo) {
     let fileName = '';
     if (packageInfo['group'].trim() != '') {
@@ -41,6 +57,12 @@ export function generatePackageName(packageInfo) {
     return fileName.charAt(fileName.length - 1) == '-' ? fileName.substring(0, fileName.length - 1) : fileName;
 }
 
+/**
+ * Generates a log message from the info of the given package depending on the given level.
+ * @param {object} packageInfo Entry from bom.json containing information for one package.
+ * @param {string} level The level of the log message.
+ * @returns {string} The message to be added to the log.
+ */
 export function generateLogMessage(packageInfo, level) {
     switch (level) {
         case 'license':
