659f3745ec96188c1e22441569a9484e4698894b
Starkiller1988 <schabel.samir@gmail.com>
2022-05-23 10:57:50 +0200

    last updates

--- src/index.js
+++ src/index.js
@@ -103,12 +103,18 @@ function insertCopyrightIntoBom(packageInfo, copyright) {
 
 function extractCopyright(license) {
   // Your code goes here. Return copyright notice as a string
+
+  const data = readFileSync("../OSSLR/regex.txt", "utf8");
+  const arrayofregex = data.split("\n");
+
+  for (let i = 0; i < arrayofregex.length; i++) {
+    let regtext = new RegExp(arrayofregex[i],'i');
+    if (regtext.test(license)) {
+      return regtext.exec(license)[0];
+    }
+  }
   return "";
 }
-
-
-
-
 
 
 //Morris Solution
--- testingRegexList.js
+++ testingRegexList.js
@@ -1,23 +1,16 @@
-import {readFileSync} from 'fs';
+
 
 function extractCopyright(license) {
-    // Your code goes here. Return copyright notice as a string
+  // Your code goes here. Return copyright notice as a string
 
+  const data = readFileSync("../OSSLR/regex.txt", "utf8");
+  const arrayofregex = data.split("\n");
 
-const data = readFileSync('../OSSLR/regex.txt', "utf8");
-const arrayofregex = data.split("\n");
-const licenceText = readFileSync('../Resources/LICENSE.txt', "utf8");
-
-for(let i = 0; i < arrayofregex.length; i++){
+  for (let i = 0; i < arrayofregex.length; i++) {
     let regtext = new RegExp(arrayofregex[i]);
-    if (regtext.test( licenceText)){
-        console.log(regtext.exec(licenceText)[0]);
-    }    
+    if (regtext.test(license)) {
+      return regtext.exec(license)[0];
+    }
+  }
+  return "";
 }
-
-    
-    return ""
-}
-
-extractCopyright('');
-
