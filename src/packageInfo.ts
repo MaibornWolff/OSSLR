0639fc9f522172f593415e51efb8a7ac2ee7d402
Starkiller1988 <schabel.samir@gmail.com>
2022-05-24 16:12:47 +0200

    created packageInfo.ts

--- src/packageInfo.ts
+++ src/packageInfo.ts
@@ -1,16 +1,21 @@
-class PackageInfo{
+class PackageInfo {
+  group: string;
+  name: string;
+  version: string;
+  licenses: object[];
+  externalReferences: object[];
 
-    group: string;
-    name: string;
-    version: string;
-    licenses: object[];
-    externalReferences: object[];
-
-    constructor(group: string, name: string, version: string, licenses: object[], externalReferences: object[]){
-        this.group = group;
-        this.name = name;
-        this.version = version;
-        this.licenses = licenses;
-        this.externalReferences = externalReferences;
-    }
+  constructor(
+    group: string,
+    name: string,
+    version: string,
+    licenses: object[],
+    externalReferences: object[]
+  ) {
+    this.group = group;
+    this.name = name;
+    this.version = version;
+    this.licenses = licenses;
+    this.externalReferences = externalReferences;
+  }
 }
