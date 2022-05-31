59c313044e9feba47dbc484bf3f79564f2e65276
Starkiller1988 <schabel.samir@gmail.com>
2022-05-31 10:58:47 +0200

    last update

--- src/export/cyclonDXExporter.ts
+++ src/export/cyclonDXExporter.ts
@@ -4,33 +4,35 @@ import { PackageInfo } from "../model/packageInfo";
 import { Exporter } from "./exporter";
 
 export class CycloneDXExporter implements Exporter {
-    exportbom(packageInfos: PackageInfo[], format: string, originalBom: string): void {
-       switch (format) {
-           case "json":
-               this.exportjson(packageInfos, originalBom);
-       }
-
-
+  exportbom(
+    packageInfos: PackageInfo[],
+    format: string,
+    originalBom: string
+  ): void {
+    switch (format) {
+      case "json":
+        this.exportjson(packageInfos, originalBom);
     }
-    exportjson(packageInfos: PackageInfo[], originalBom: string) {
-        originalBom = JSON.parse(originalBom);
-        originalBom = this.insertCopyrightIntoBom(packageInfos, originalBom)
-        try {
-            writeFileSync(path.join('out', 'updatedbom.json'), JSON.stringify(originalBom, null, 4));
-        } catch (err) {
-            console.error(err);
-        }
+  }
+  exportjson(packageInfos: PackageInfo[], originalBom: string) {
+    originalBom = JSON.parse(originalBom);
+    originalBom = this.insertCopyrightIntoBom(packageInfos, originalBom);
+    try {
+      writeFileSync(
+        path.join("out", "updatedbom.json"),
+        JSON.stringify(originalBom, null, 4)
+      );
+    } catch (err) {
+      console.error(err);
     }
-    insertCopyrightIntoBom(packageInfos: PackageInfo[], originalBom: string) {
-        for(let i = 0; i < packageInfos.length; i++) {
-            let copyright = packageInfos[i].copyright;
-            if(copyright !== "") {
-                originalBom["components"][i]["copyright"] = copyright;
-            }
-        }   
-        return originalBom;
+  }
+  insertCopyrightIntoBom(packageInfos: PackageInfo[], originalBom: string) {
+    for (let i = 0; i < packageInfos.length; i++) {
+      let copyright = packageInfos[i].copyright;
+      if (copyright !== "") {
+        originalBom["components"][i]["copyright"] = copyright;
+      }
     }
-
+    return originalBom;
+  }
 }
-
-
