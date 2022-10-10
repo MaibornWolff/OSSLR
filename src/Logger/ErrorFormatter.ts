41e1dbe16fb36a3d8d5236062978e41856a71b95
Morris Witthein <morris.witthein@maibornwolff.de>
2022-10-10 14:57:09 +0200

    Fix Type issue

--- src/Logger/ErrorFormatter.ts
+++ src/Logger/ErrorFormatter.ts
@@ -1,4 +1,5 @@
-import chalk from 'Chalk';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import chalk from 'chalk';
 
 export function printError(errorMessage: any) {
     console.error(chalk.red(errorMessage));
